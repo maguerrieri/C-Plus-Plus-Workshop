5e9e61e6054f41acf2a783f6dd802c6d0a20cce9
Mario Guerrieri <mario@guerrieri.codes>
2021-09-09 18:46:34 -0700

    Second session demo 3

--- Sources/C++Workshop/Objects.h
+++ Sources/C++Workshop/Objects.h
@@ -11,7 +11,21 @@
 
 // The only difference between structs and classes is that
 // structs' members are public by default and classes' are private.
-struct account {
-    std::string holder_name;
-    double balance;
+class account {
+public:
+    // See https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#p1-express-ideas-directly-in-code
+    struct currency { double amount; };
+    struct holder { std::string name; };
+    
+    // See https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#c40-define-a-constructor-if-a-class-has-an-invariant
+    // and subsequent guidelines
+    account(const holder& holder, const currency& balance): _holder{holder}, _balance{balance} {
+        if (this->_holder.name.size() == 0 || this->_balance.amount < 0) {
+            throw std::runtime_error{"Invalid account parameters"};
+        }
+    }
+    
+private:
+    holder _holder;
+    currency _balance;
 };
--- Tests/C++WorkshopTests/Objects.mm
+++ Tests/C++WorkshopTests/Objects.mm
@@ -18,10 +18,18 @@
 - (void)testExample {
     // Should be familiar from Python, other modern languages (Kotlin, Swift, Rust).
     // Note the curly braces { }; see https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Res-list
-    auto some_account = account{};
+//    auto some_account = account{};
     
     // Simple objects like this one can be constructed with a designated initializer
-    auto initialized_account = account{ .holder_name = "Mario", .balance = 1'000'000 };
+//    auto initialized_account = account{ .holder_name = "Mario", .balance = 1'000'000 };
+    
+    // But as types get more complicated (and need to enforce invariants), they should provide a constructor
+    try {
+        auto new_account = account{{ .name = "Mario" }, { .amount = 1'000'000 }};
+        auto other_account = account{{ .name = "Mario" }, { .amount = -1000 }};
+    } catch (const std::exception&) {
+        XCTFail(@"Oops!");
+    }
 }
 
 @end
