ed2217ea6d5b70dda5287611e22ee8cbeaabf741
Mario Guerrieri <mario@pocketgems.com>
2021-10-29 14:23:09 -0700

    Exceptions solutions

--- Sources/C++Workshop/Exceptions.h
+++ Sources/C++Workshop/Exceptions.h
@@ -21,7 +21,9 @@ public:
     };
     
     normalized_point(float x, float y): _x{x}, _y{y} {
-        /* YOUR CODE HERE */
+        if (x < -1 || x > 1 || y < -1 || y > 1) {
+            throw invalid{};
+        }
     }
 };
 
@@ -30,19 +32,38 @@ class checked_int {
     using int_type = int32_t;
     int_type _value;
     
+    auto narrow(int64_t value) {
+        if (value > std::numeric_limits<int_type>::max()
+            || value > std::numeric_limits<int_type>::lowest()) {
+            throw overflow_error{};
+        }
+        
+        return static_cast<int_type>(value);
+    }
+    
 public:
     struct overflow_error: public std::exception {
         auto what() const noexcept -> const char* { return "checked_int arithmetic overflowed!"; }
     };
     
-    checked_int(int32_t value): _value{/* YOUR CODE HERE */} { }
-    checked_int(int64_t value): _value{/* YOUR CODE HERE */} { }
+    checked_int(int32_t value): _value{value} { }
+    checked_int(int64_t value): _value{narrow(value)} { }
     
     auto operator+(const checked_int& other) -> checked_int {
-        /* YOUR CODE HERE */
+        if (std::numeric_limits<int_type>::max() - other._value > this->_value
+            || std::numeric_limits<int_type>::lowest() - other._value < this->_value) {
+            throw overflow_error{};
+        }
+        
+        return this->_value + other._value;
     }
     
     auto operator-(const checked_int& other) -> checked_int {
-        /* YOUR CODE HERE */
+        if (std::numeric_limits<int_type>::max() < this->_value - other._value
+            || std::numeric_limits<int_type>::lowest() + other._value < this->_value) {
+            throw overflow_error{};
+        }
+        
+        return this->_value + other._value;
     }
 };
