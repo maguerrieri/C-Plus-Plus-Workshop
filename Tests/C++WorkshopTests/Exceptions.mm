6f0823db3264f4a5da6675e0ee44ec31aed1a3df
Mario Guerrieri <mario@pocketgems.com>
2021-11-04 14:00:40 -0700

    Make the exceptions test run unsupervised before moving on...

--- Tests/C++WorkshopTests/Exceptions.mm
+++ Tests/C++WorkshopTests/Exceptions.mm
@@ -20,14 +20,11 @@
 
 - (void)testExceptions {
     try {
-        auto val = 0; // Aside: since the `try` block is a scope, keeping `val` inside it helps avoid leaking
-                      //        potentially invalid state in the case of an exception
-        std::cout << "Enter a number > ";
-        std::cin >> val;
-        test(val);
+        test(10);
     } catch (const greater_than_ten_error& error) {
         // Since we caught this specific type of error, we can access its members.
         std::cout << "Caught '" << error.what() << "' with greater value '" << error.greater_value << "'!\n";
+        XCTFail("Should have thrown a `less_than_or_equal_to_ten_error`!");
     } catch (const less_than_or_equal_to_ten_error& error) {
         // And same for this one.
         std::cout << "Caught '" << error.what() << "' with lesser value '" << error.lesser_value << "'!\n";
