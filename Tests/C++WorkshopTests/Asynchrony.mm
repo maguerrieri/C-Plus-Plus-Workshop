d8d0eddfed7f42e3f14734cd2cfb980cbf4c35cb
Mario Guerrieri <mario@pocketgems.com>
2021-11-05 13:50:20 -0700

    Extra stuff from session

--- Tests/C++WorkshopTests/Asynchrony.mm
+++ Tests/C++WorkshopTests/Asynchrony.mm
@@ -18,8 +18,11 @@
 @implementation AsynchronyTests
 
 - (void)testAsynchrony {
+    auto executor = cf::async_thread_pool_executor{1};
+    auto executor_2 = cf::async_thread_pool_executor{1};
+    
     // Should be at least vaguely familiar from my past presentations/docs about asynchrony
-    cf::async([] {
+    auto future = cf::async(executor, [] {
         // The default default executor for `cf` is a `std::thread`
         std::cout << "Hello from the default executor!\n";
         
@@ -28,10 +31,38 @@
     .then([](auto unit_future) {
         // Runs sequentially, on the same executor (in this case, the same thread that was created for the initial
         // `async` block)
-        std::cout << "Hello from a continuation on the default executor!\n";
+        std::cout << "Hello from a continuation on the same executor!\n";
         
         return cf::unit{};
-    }).get();
+    })
+    .then(executor_2, [](auto future) {
+        std::cout << "Hello from a continuation on a different executor!\n";
+        
+        return cf::unit{};
+    })
+    .then([](auto unit_future) {
+        // Runs sequentially, on the same executor (in this case, the same thread that was created for the initial
+        // `async` block)
+        std::cout << "Hello from a continuation on the same thread executor!\n";
+        
+        return cf::unit{};
+    })
+    .then(executor, [](auto unit_future) {
+        // Runs sequentially, on the same executor (in this case, the same thread that was created for the initial
+        // `async` block)
+        std::cout << "Hello from a continuation on the thread executor!\n";
+        
+        throw std::runtime_error{"oops!"};
+        return cf::unit{};
+    });
+    
+    auto future_2 = cf::future<cf::unit>{};
+    
+    try {
+        future_2.get();
+    } catch (const std::exception& e) {
+        std::cout << "Caught '" << e.what() << "'!\n";
+    }
 }
 
 @end
